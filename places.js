e5ed8003d2f86a5beb57c46c50221887b98d43c0
fujiwarakunihiko <fujiwara.kunihiko@takenaka.co.jp>
2020-10-06 18:32:56 +0900

    Update places.js

--- places.js
+++ places.js
@@ -6,8 +6,8 @@ const loadPlaces = function(coords) {
         {
             name: "Your place name",
             location: {
-                lat: 0, // add here latitude if using static data
-                lng: 0, // add here longitude if using static data
+                lat: 35.8044, // add here latitude if using static data
+                lng: 140.1193, // add here longitude if using static data
 
             }
         },
